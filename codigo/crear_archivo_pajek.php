214008bdeab0eee774ffe57b780595abece167f8
root <root@deb-memoria>
2015-01-25 23:18:50 -0300

    se agregan archivos procesados

---
+++ codigo/crear_archivo_pajek.php
@@ -0,0 +1,75 @@
+<?php
+
+$RUTA_PAJEK   = '/tmp/'; 
+$NOMBRE_PAJEK = 'pajek.net';
+
+if ($argc !== 2) {
+  echo "Error: Debe ingresar la ruta (absoluta o relativa) del archivo CSV.".PHP_EOL;
+  exit(1);
+}
+
+$ruta_csv    = $argv[1];
+
+// indica si el archivo existe y si es posible leerlo
+if (!is_readable($ruta_csv)) {
+  echo "El archivo no existe o no se puede leer".PHP_EOL;
+  exit(1);
+}
+
+$archivo_csv   = fopen($ruta_csv, 'r');
+$archivo_pajek = fopen($RUTA_PAJEK . $NOMBRE_PAJEK, 'w');
+
+if ($archivo_pajek === false) {
+  echo "No se pudo crear archivo $NOMBRE_PAJEK".PHP.EOL;
+  exit(1);
+}
+
+// paraderos encontrados
+$paraderos = Array();
+
+$total_arcos = 0;
+
+// cadena de texto
+$arcos    = '';
+$vertices = '';
+
+// se lee linea por linea
+while (($linea = fgets($archivo_csv)) !== false) {
+
+  $elementos = array_map("trim", split(",", $linea));
+
+  // si no está en el arreglo => se agrega y se obtiene el indice
+  if (($indice_par_subida = array_search($elementos[0], $paraderos)) === false) {
+    $paraderos[] = $elementos[0];
+    $indice_par_subida = count($paraderos);
+    
+    $vertices .= $indice_par_subida . ' "' . $elementos[0] . '"' . PHP_EOL;
+  } else {
+    $indice_par_subida++;
+  }
+
+  if (($indice_par_bajada = array_search($elementos[1], $paraderos)) === false) {
+    $paraderos[] = $elementos[1];
+    $indice_par_bajada = count($paraderos);
+    
+    $vertices .= $indice_par_bajada . ' "' . $elementos[1] . '"' .  PHP_EOL;
+  } else {
+    $indice_par_bajada++;
+  }
+  
+  $arcos .= $indice_par_subida . ' ' . $indice_par_bajada . ' ' . $elementos[2] . PHP_EOL;
+  $total_arcos++;
+}
+
+fclose($archivo_csv);
+
+$total_nodos = count($paraderos);
+
+// se construye archivo pajek
+fwrite($archivo_pajek, "*Vertices " . $total_nodos . PHP_EOL);
+fwrite($archivo_pajek, $vertices);
+fwrite($archivo_pajek, "*Edges " . $total_arcos . PHP_EOL);
+fwrite($archivo_pajek, $arcos);
+
+fclose($archivo_pajek);
+?>
---
+++ codigo/poner_nombre_paradero.php
@@ -0,0 +1,52 @@
+<?php
+
+$RUTA_PAJEK   = '/tmp/'; 
+$NOMBRE_PAJEK = 'pajek_paraderos_con_nombre.net';
+
+if ($argc !== 3) {
+  echo "USO: php poner_nombre_paradero.php <ruta archivo pajek> <ruta archivo nombre paraderos>".PHP_EOL;
+  exit(1);
+}
+
+$ruta_pajek_con_datos  = $argv[1];
+$ruta_nombre_paraderos = $argv[2];
+
+// indica si el archivo existe y si es posible leerlo
+if (!is_readable($ruta_nombre_paraderos)) {
+  echo "El archivo '$ruta_nombre_paraderos' no existe o no se puede leer".PHP_EOL;
+  exit(1);
+}
+
+// indica si el archivo existe y si es posible leerlo
+if (!is_readable($ruta_pajek_con_datos)) {
+  echo "El archivo '$ruta_pajek_con_datos' no existe o no se puede leer".PHP_EOL;
+  exit(1);
+}
+
+$contenido_pajek_con_datos = file_get_contents($ruta_pajek_con_datos, FILE_USE_INCLUDE_PATH);
+
+$archivo_nombre_paraderos = fopen($ruta_nombre_paraderos, 'r');
+$archivo_pajek = fopen($RUTA_PAJEK . $NOMBRE_PAJEK, 'w');
+
+if ($archivo_nombre_paraderos === false) {
+  echo "No se pudo crear archivo $archivo_nombre_paraderos".PHP.EOL;
+  exit(1);
+}
+
+if ($archivo_pajek === false) {
+  echo "No se pudo crear archivo $RUTA_PAJEK/$NOMBRE_PAJEK".PHP.EOL;
+  exit(1);
+}
+
+// se lee linea por linea
+while (($linea = fgets($archivo_nombre_paraderos)) !== false) {
+  $elementos = array_map("trim", split(";", $linea));
+
+  $contenido_pajek_con_datos = str_replace($elementos[0], $elementos[3], $contenido_pajek_con_datos);
+}
+
+fclose($archivo_nombre_paraderos);
+
+fwrite($archivo_pajek, $contenido_pajek_con_datos);
+fclose($archivo_pajek);
+?>
