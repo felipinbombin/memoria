53af5b496d361487310c7728da5b4a38d698e773
Felipe <cephei.1313@gmail.com>
2015-03-18 11:39:32 -0300

    se automatiza carga de datos

--- codigo/archivo_bash.sh
+++ codigo/archivo_bash.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-PATH=/home/cephei/Desktop/backup
+# Ruta de los archivos con los datos de la base de datos abril-2013.
+RUTA_DATOS=/home/cephei/Desktop/backup
+RUTA_MEMORIA=/home/cephei/Desktop/memoria
+RUTA_CODIGO=$RUTA_MEMORIA/codigo
 
-# cambiamos al usuario postgres
-su postgres 
+# Crear base de datos y usuario
+sudo -u postgres -i psql -f $RUTA_CODIGO/crear_db.sql
+
+# Crear la estructura de la base de datos.
+sudo -u postgres -i psql -d memoria -f $RUTA_CODIGO/estructura_db.sql
+
+echo "Importando datos..."
 
 # Se cargan los datos en la base de datos
-psql -d memoria -a -f $PATH/backup_etapas
-psql -d memoria -a -f $PATH/backup_viajes
-psql -d memoria -a -f $PATH/backup_redparadas
+sudo -u postgres -i psql -d memoria -a -f $RUTA_DATOS/backup_etapas
+sudo -u postgres -i psql -d memoria -a -f $RUTA_DATOS/backup_viajes
+sudo -u postgres -i psql -d memoria -a -f $RUTA_DATOS/backup_redparadas
 
+echo "Datos importados exitosamente."
 
+# Se lleva a cabo el filtrado de datos
 
-#echo id
-
-#psql sigge
-
-#\q
-
-# la opción -S de sudo permite leer la contraseña desde STD
-
-#su root
-
+#sudo -u postgres -i psql -d memoria -f $RUTA_CODIGO/filtrar_datos.sql
---
+++ codigo/crear_db.sql
@@ -0,0 +1,9 @@
+
+-- se elimina la base de datos existente.
+DROP DATABASE IF EXISTS memoria;
+DROP USER IF EXISTS felipe;
+
+-- Se crea usuario y base de datos
+CREATE USER felipe WITH PASSWORD 'felipe';
+CREATE DATABASE memoria OWNER felipe;
+
