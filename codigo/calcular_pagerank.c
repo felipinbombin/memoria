0ba93164ae5c47bfc37b7fc84bb1eaefa886866a
Felipe <cephei.1313@gmail.com>
2015-04-03 19:27:35 -0300

    se calcula el pagerank con igraph y se genera un csv con las columnas (paradero_id pagerank)

--- codigo/calcular_pagerank.c
+++ codigo/calcular_pagerank.c
@@ -1,126 +1,94 @@
-#include <igraph.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+//#include <stdlib.h>
+//#include <string.h>
+#include <igraph.h>
 
-#define true  1
-#define false 0
+void print_vector(igraph_vector_t *v, FILE *f) {
+  long int i;
+  for(i=0; i < igraph_vector_size(v); i++) {
+    fprintf(f, " %f", VECTOR(*v)[i]);
+  }
+  fprintf(f, "\n");
+}
 
 int main(int argc, char *argv[])
 {
-  /* 
-   * Se lee archivo pajek.
-   * Desde aquí se obtiene la cantidad de nodos, 
-   * sus ids y los arcos entre ellos con su respectivo peso.
-   */
-  igraph_integer_t num_nodos;
-  igraph_vector_t arcos;
-
-  FILE* archivo_pajek;
+  igraph_t grafo;
+  // Vector con el pagerank de cada nodo
+  igraph_vector_t resultado;
+  igraph_real_t valor = 1;
+  // Iterador
+  long int i;
+  // Vector con los pesos de cada arco
+  igraph_vector_t pesos;
+  // Archivo pajek
+  FILE *archivo_pajek;
+  
+  // handler para manipular características del archivo pajek
+  igraph_i_set_attribute_table(&igraph_cattribute_table);
   
   archivo_pajek= fopen(argv[1], "rt");
   /* "rt" significa abrir un archivo para leer texto */
 
-  int n_linea = 0;
-  int n_nodos = 0;
-  int n_arcos = 0;
-  int fila_es_nodo = true;
-
-  // ids de nodos
-  int nodo, nodo1, nodo2;
-  float peso;
-
-  char linea[80];
-
-  while(fgets(linea, 80, archivo_pajek) != NULL)
-  {
-    // la primera linea contiene la cantidad de nodos.
-    if (n_linea == 0) 
-    {
-      sscanf (linea, "%*[^0-9]%d", &n_nodos);
-      //printf("nodos: %d\n", n_nodos);
-    } 
-    // llegamos al encabezado de los arcos
-    else if (n_linea == 1 + n_nodos) 
-    {
-      sscanf (linea, "%*[^0-9]%d", &n_arcos);
-      fila_es_nodo = false;
-      //printf("arcos: %d\n", n_arcos);
-    } 
-    // fila con datos
-    else 
-    {
-      // es un nodo
-      if (fila_es_nodo) 
-      {
-        sscanf (linea, "%d ", &nodo);
-        //printf("nodo: %d\n", nodo);
-      } 
-      // es un arco
-      else 
-      {
-        sscanf (linea, "%d %d %f", &nodo1, &nodo2, &peso);
-        //printf("arco (%d,%d)=%f\n", nodo1, nodo2, peso);
-      }
-    }
-
-    // vamos a la siguiente línea del archivo
-    n_linea++;
+  if(igraph_read_graph_pajek(&grafo, archivo_pajek) != 0) {
+    printf("ERROR: no se pudo cargar el archivo.");
+    return 1;
   }
 
   fclose(archivo_pajek);
-  return 0;
 
-  /*
-   * Creación de grafo en igraph
-   */
-/*
-  igraph_t grafo;
+  // ************************************************
+  igraph_vector_t gtypes, vtypes, etypes;
+  igraph_strvector_t gnames, vnames, enames;
+  
+  igraph_vector_init(&gtypes, 0);
+  igraph_vector_init(&vtypes, 0);
+  igraph_vector_init(&etypes, 0);
 
+  igraph_strvector_init(&gnames, 0);
+  igraph_strvector_init(&vnames, 0);
+  igraph_strvector_init(&enames, 0);
 
+  igraph_cattribute_list(&grafo, &gnames, &gtypes, &vnames, &vtypes, &enames, &etypes);
 
-  // se crea un grado vacío
-  igraph_empty(&grafo, num_nodos, IGRAPH_DIRECTED);
+  igraph_vector_init(&pesos, 0);
 
-  // se crea un vector de arcos.
-  igraph_add_edges(&grafo, &arcos);
+  // se crea el vector de pesos
+  for (i=0; i<igraph_ecount(&grafo); i++) {
+     //printf("%s=", STR(enames, 0));
+     //igraph_real_printf(EAN(&grafo, STR(enames, 0), i));
+     //putchar('\n');
+     // se agregan los pesos al vector
+     igraph_vector_insert(&pesos, i, EAN(&grafo, STR(enames, 0), i));
+  }
 
-  // se destruye el grafo
+  igraph_vector_init(&resultado, 0);
+
+  if(igraph_pagerank(&grafo, IGRAPH_PAGERANK_ALGO_PRPACK, 
+        &resultado, &valor, igraph_vss_all(), igraph_is_directed(&grafo), 0.85, &pesos, NULL) != 0) {
+    printf("ERROR: no se pudo calcular el pagerank.");
+    return 1;
+  }
+
+  //printf("El grafo tiene %d vertices y %d arcos\n", igraph_vcount(&grafo), igraph_ecount(&grafo));
+  //printf("cantidad de pagerank: %d", igraph_vector_size(&resultado));
+  //print_vector(&resultado, stdout);
+  
+  // Se imprime csv (id_paradero, pagerank)
+  printf("paradero_id pagerank\n");
+  for (i=0; i<igraph_vcount(&grafo); i++) {
+    printf("\"%s\" %f\n", VAS(&grafo, STR(vnames,0), i), VECTOR(resultado)[i]);
+  }
+
+  igraph_strvector_destroy(&enames);
+  igraph_strvector_destroy(&vnames);
+  igraph_strvector_destroy(&gnames);
+
+  igraph_vector_destroy(&etypes);
+  igraph_vector_destroy(&vtypes);
+  igraph_vector_destroy(&gtypes);
+
   igraph_destroy(&grafo);
+
   return 0;
-
-  // tipo de dato entero para igraph
-  igraph_integer_t diameter;
-  // tipo de dato real para igraph
-  igraph_real_t a;
-  // vector de arcos
-  igraph_vector_t dimension;
-  igraph_vector_t arcos;
-
-  // crea los vectores
-  igraph_vector_init(&dimension, 2);
-  igraph_vector_init(&arcos, 10000);
-
-  VECTOR(dimension)[0]=30;
-  VECTOR(dimension)[1]=30;
-
-  igraph_vector_size(&arcos);
-  igraph_add_edges();
-
-  // configura la semilla usada para la generación de números aleatorios
-  igraph_rng_seed(igraph_rng_default(), 42);
-
-
-  // crea un grafo
-  igraph_erdos_renyi_game(&graph, IGRAPH_ERDOS_RENYI_GNP, 1000, 5.0/1000, IGRAPH_UNDIRECTED, IGRAPH_NO_LOOPS);
-  igraph_diameter(&graph, &diameter, 0, 0, 0, IGRAPH_UNDIRECTED, 1);
-
-  printf("Diameter of a random graph with average degree 5: %d\n", (int) diameter);
-
-  igraph_vector_destroy(&arcos);
-  igraph_vector_destroy(&dimension);
-  // destruye un grafo (liberando memoria)
-  igraph_destroy(&graph);
-  return 0;
-  */
 }
