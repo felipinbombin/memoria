ef1c2621bfeff1c5f7dbb5fbde12a0586079041b
Felipe <cephei.1313@gmail.com>
2015-05-06 12:22:39 -0300

    se agrega opción para dibujar poligonos

--- codigo/generar_comunidades.sh
+++ codigo/generar_comunidades.sh
@@ -14,9 +14,11 @@ GENERAR_PAJEK=false
 # crea el csv de paradas con toda su información (nombre, longitud, latitud, ...)
 GENERAR_CSV_PARADAS=false
 # genera las comunidades en dos niveles, por medio del framework infomap
-GENERAR_COMUNIDADES=false
+GENERAR_COMUNIDADES=true
 # genera un csv a partir del archivo de comunidades para poder ser mostrado en la herramienta cartodb.com 
 GENERAR_CARTODB=true
+# genera poligonos a partir de las comunidades.
+GENERAR_POLIGONOS=false
 
 ####################################################################################
 # Ruta de los directorios usados por el script
@@ -27,6 +29,7 @@ RUTA_DATOS_CSV=$RUTA_DATOS/csv
 RUTA_DATOS_PAJEK=$RUTA_DATOS/pajek
 RUTA_DATOS_INFOMAP=$RUTA_DATOS/infomap
 RUTA_DATOS_CARTODB=$RUTA_DATOS/cartodb
+RUTA_DATOS_POLIGONO=$RUTA_DATOS/poligono
 RUTA_INFOMAP=/home/cephei/Desktop/Infomap
 ####################################################################################
 
@@ -47,13 +50,35 @@ chmod 775 -R $RUTA_DATOS
 
 if [ "$GENERAR_ETAPA_CSV" = true ]; then
   ####################################################################################
-  rm -f $RUTA_DATOS_CSV/*etapa.csv
+  #rm -f $RUTA_DATOS_CSV/*etapa.csv
 
   # Se ocupan los datos de toda la semana. 
-  CONSULTA="copy (SELECT par_subida, par_bajada, SUM(factor_expansion) AS peso 
-                  FROM etapa_util 
-                  GROUP BY par_subida, par_bajada) 
-                  To '$RUTA_DATOS_CSV/semana_etapa.csv' WITH DELIMITER ';' CSV;"
+#  CONSULTA="copy (SELECT par_subida, par_bajada, SUM(factor_expansion) AS peso 
+#                  FROM etapa_util 
+#                  GROUP BY par_subida, par_bajada) 
+#                  To '$RUTA_DATOS_CSV/semana_etapa.csv' WITH DELIMITER ';' CSV;"
+    CONSULTA="copy (SELECT par_subida, par_bajada, SUM(peso) 
+                    FROM (SELECT par_subida_1 AS par_subida, par_bajada_1 AS par_bajada, SUM(factor_expansion) AS peso
+                          FROM viaje_util 
+                          WHERE netapa=1  
+                          GROUP BY par_subida_1, par_bajada_1  
+                          UNION 
+                          SELECT par_subida_1, par_bajada_2, SUM(factor_expansion) AS peso 
+                          FROM viaje_util 
+                          WHERE netapa=2  
+                          GROUP BY par_subida_1, par_bajada_2 
+                          UNION 
+                          SELECT par_subida_1, par_bajada_3, SUM(factor_expansion) AS peso 
+                          FROM viaje_util 
+                          WHERE netapa=3  
+                          GROUP BY par_subida_1, par_bajada_3 
+                          UNION 
+                          SELECT par_subida_1, par_bajada_4, SUM(factor_expansion) AS peso 
+                          FROM viaje_util 
+                          WHERE netapa=4 
+                          GROUP BY par_subida_1, par_bajada_4) AS viaje 
+                    GROUP BY par_subida, par_bajada) 
+              To '$RUTA_DATOS_CSV/semana_viaje.csv' WITH DELIMITER ';' CSV;"
 
   sudo -u postgres -i psql -d memoria -c "$CONSULTA"
 
@@ -121,7 +146,7 @@ fi
 
 if [ "$GENERAR_PAJEK" = true ]; then
   ####################################################################################
-  rm -f $RUTA_DATOS_PAJEK/*.net
+  #rm -f $RUTA_DATOS_PAJEK/*.net
 
   for ARCHIVO_CSV in $RUTA_DATOS_CSV/*.csv; do
     echo "Procesando $ARCHIVO_CSV"
@@ -143,7 +168,7 @@ fi
 
 if [ "$GENERAR_COMUNIDADES" = true ]; then
   ####################################################################################
-  rm -f $RUTA_DATOS_INFOMAP/*.tree
+  #rm -f $RUTA_DATOS_INFOMAP/*.tree
 
   for ARCHIVO_NET in $RUTA_DATOS_PAJEK/*.net; do
     echo "Procesando $ARCHIVO_NET"
@@ -154,19 +179,43 @@ if [ "$GENERAR_COMUNIDADES" = true ]; then
     # --two-level  Optimiza una partición de dos niveles de la red
     # -d           Asume que los arcos tienen dirección
     # --zero-based Considera que la enumeración de los indices comienza desde cero.
-    $RUTA_INFOMAP/Infomap -i 'pajek' --two-level -d "$ARCHIVO_NET" "$RUTA_DATOS_INFOMAP"
+    # --code-rate cantidad de pasos que realiza el peatón antes de ser codificada su acción.
+    $RUTA_INFOMAP/Infomap --code-rate 1 -i 'pajek' --two-level -d "$ARCHIVO_NET" "$RUTA_DATOS_INFOMAP"
   done 
 fi
 
 if [ "$GENERAR_CARTODB" = true ]; then
   ####################################################################################
-  rm -f -R $RUTA_DATOS_CARTODB/*.csv
+  #rm -f -R $RUTA_DATOS_CARTODB/*.csv
 
   for ARCHIVO_TREE in $RUTA_DATOS_INFOMAP/*.tree; do
     echo "Procesando $ARCHIVO_TREE"
 
-    php $RUTA_CODIGO/tree2csv.php $RUTA_DATOS/$NOMBRE_PARADAS_CSV $ARCHIVO_TREE $RUTA_DATOS_CARTODB/ 
+    php $RUTA_CODIGO/tree2cartodb.php $RUTA_DATOS/$NOMBRE_PARADAS_CSV $ARCHIVO_TREE $RUTA_DATOS_CARTODB/ 
   done
+fi
+
+if [ "$GENERAR_POLIGONOS" = true ]; then
+  ####################################################################################
+  #rm -f -R $RUTA_DATOS_POLIGONO/*.csv
+
+  NOMBRE_POLIGONO_C="crear_poligono"
+  NOMBRE_CONVEX_HULL_C="2dch"
+  #compilar programa para calcular la covertura convexa
+  gcc $NOMBRE_CONVEX_HULL_C.c -o $NOMBRE_CONVEX_HULL_C
+  #compilar programa para generar shapefile
+  gcc $NOMBRE_POLIGONO_C.c -o $NOMBRE_POLIGONO_C
+
+  for ARCHIVO_CSV in $RUTA_DATOS_CARTODB/*.csv; do
+    echo "Procesando $ARCHIVO_CSV"
+
+    INPUT=$(php $RUTA_CODIGO/csv2poligono.php $ARCHIVO_CSV)
+    $RUTA_CODIGO/$NOMBRE_ARCHIVO_C $RUTA_DATOS_POLIGONO $INPUT
+  done
+
+  #eliminar ejecutables
+  rm -f $RUTA_CODIGO/$NOMBRE_POLIGONO_C
+  rm -f $RUTA_CODIGO/$NOMBRE_CONVEX_HULL_C
 fi
 
 # cambiamos el dueño de los archivos para poder verlos en el entorno de escritorio
