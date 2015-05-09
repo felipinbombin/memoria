ac66af0d4de0d97adb049ae2332c49886c71234a
Felipe <cephei.1313@gmail.com>
2015-05-09 11:02:58 -0300

    se agregan scripts para calculo de centralidad de intermediación teórica (betwenness centrality)

---
+++ codigo/centralidad_teoria/calcular_centralidad_teorica.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <igraph.h>
+
+void print_vector(igraph_vector_t *v, FILE *f) {
+  long int i;
+  for(i=0; i < igraph_vector_size(v); i++) {
+    // se imprime solo valores negativos
+    if (VECTOR(*v)[i] <= 0)
+      fprintf(f, "valor: %f | indice: %d", VECTOR(*v)[i], i);
+  }
+  fprintf(f, "\n");
+}
+
+int main(int argc, char *argv[])
+{
+  igraph_t grafo;
+  // Vector con el betweeness centrality de cada nodo
+  igraph_vector_t resultado;
+  igraph_real_t valor = 1;
+  // Iterador
+  long int i;
+  // Vector con los pesos de cada arco
+  igraph_vector_t pesos;
+  // Archivo pajek
+  FILE *archivo_pajek;
+  
+  // handler para manipular características del archivo pajek
+  igraph_i_set_attribute_table(&igraph_cattribute_table);
+  
+  archivo_pajek= fopen(argv[1], "rt");
+  /* "rt" significa abrir un archivo para leer texto */
+
+  if(igraph_read_graph_pajek(&grafo, archivo_pajek) != 0) {
+    printf("ERROR: no se pudo cargar el archivo.");
+    return 1;
+  }
+
+  fclose(archivo_pajek);
+
+  // ************************************************
+  igraph_vector_t gtypes, vtypes, etypes;
+  igraph_strvector_t gnames, vnames, enames;
+  
+  igraph_vector_init(&gtypes, 0);
+  igraph_vector_init(&vtypes, 0);
+  igraph_vector_init(&etypes, 0);
+
+  igraph_strvector_init(&gnames, 0);
+  igraph_strvector_init(&vnames, 0);
+  igraph_strvector_init(&enames, 0);
+
+  igraph_cattribute_list(&grafo, &gnames, &gtypes, &vnames, &vtypes, &enames, &etypes);
+
+  igraph_vector_init(&pesos, 0);
+
+  // se crea el vector de pesos
+  for (i=0; i<igraph_ecount(&grafo); i++) {
+     //printf("%s=", STR(enames, 0));
+     //igraph_real_printf(EAN(&grafo, STR(enames, 0), i));
+     //putchar('\n');
+     // se agregan los pesos al vector
+     igraph_vector_insert(&pesos, i, EAN(&grafo, STR(enames, 0), i));
+  }
+  
+  // obtener peso máximo de los pesos
+  igraph_real_t max_peso = igraph_vector_max(&pesos);
+
+  // vector con pesos contrarrestados para que aquellos pesos más altos sean los más bajos y
+  // viceversa dado que el algoritmo betweeness centrality considera el peso como una medida
+  // negativa.
+  igraph_vector_t peso_complemento;
+  igraph_vector_init(&peso_complemento, igraph_vector_size(&pesos));
+  // se suma 1 porque el 0.00 queda como -0.00 y se lanza error por peso negativo
+  igraph_vector_fill(&peso_complemento, max_peso+1);
+  igraph_vector_sub(&pesos, &peso_complemento);
+  // para que quede positivo
+  igraph_vector_scale(&pesos, -1);
+
+  //fprintf(stderr, "El indice con peso máximo (%f) es %d\n", max_peso, igraph_vector_which_max(&pesos));
+  //print_vector(&pesos, stderr);
+
+  igraph_vector_init(&resultado, 0);
+
+  if(igraph_betweenness(&grafo, &resultado, igraph_vss_all(), igraph_is_directed(&grafo), &pesos, 1) != 0) {
+    printf("ERROR: no se pudo calcular el betweeness centrality.");
+    return 1;
+  }
+
+  //printf("El grafo tiene %d vertices y %d arcos\n", igraph_vcount(&grafo), igraph_ecount(&grafo));
+  //printf("cantidad de elementos en vector de resultados: %d", igraph_vector_size(&resultado));
+  //print_vector(&resultado, stdout);
+  
+  // Se imprime csv (id_paradero, CdI)
+  printf("paradero_id,CdI\n");
+  for (i=0; i<igraph_vcount(&grafo); i++) {
+    printf("\"%s\",%f\n", VAS(&grafo, STR(vnames,0), i), VECTOR(resultado)[i]);
+  }
+
+  // da lo mismo porque se termina el script 
+  
+  igraph_strvector_destroy(&enames);
+  igraph_strvector_destroy(&vnames);
+  igraph_strvector_destroy(&gnames);
+
+  igraph_vector_destroy(&etypes);
+  igraph_vector_destroy(&vtypes);
+  igraph_vector_destroy(&gtypes);
+
+  igraph_destroy(&grafo);
+
+  return 0;
+}
---
+++ codigo/centralidad_teoria/centralidad2cartodb.php
@@ -0,0 +1,67 @@
+<?php
+// convierte un archivo .csv (paradero_id centralidad) a un csv para ser mostrado en cartodb.
+
+if ($argc < 3) {
+  echo "USO: php centralidad2cartodb.php <ruta archivo nombre paraderos> <ruta archivo csv> <ruta archivo salida> <hora datos>".PHP_EOL;
+  exit(1);
+}
+
+// archivo csv con columnas codigo, nombre, longitud, latitud
+$ruta_nombre_paraderos = $argv[1]; 
+// ruta al archivo a ser leído.
+$ruta_csv_centralidad  = $argv[2]; 
+// nombre del archivo de salida
+$nombre_csv            = array_shift(explode('.', array_pop(explode('/', $ruta_csv_centralidad)))).'.csv'; 
+// ruta donde se almacena el archivo de salida
+$ruta_salida           = $argv[3]; 
+// hora de los datos (opcional)
+$hora                  = (isset($argv[4])?$argv[4]:'');
+
+// indica si el archivo existe y si es posible leerlo
+if (!is_readable($ruta_nombre_paraderos)) {
+  echo "El archivo '$ruta_nombre_paraderos' no existe o no se puede leer".PHP_EOL;
+  exit(1);
+}
+
+// indica si el archivo existe y si es posible leerlo
+if (!is_readable($ruta_csv_centralidad)) {
+  echo "El archivo '$ruta_csv_centralidad' no existe o no se puede leer".PHP_EOL;
+  exit(1);
+}
+
+$contenido_csv = file_get_contents($ruta_csv_centralidad, FILE_USE_INCLUDE_PATH);
+
+$archivo_nombre_paraderos = fopen($ruta_nombre_paraderos, 'r');
+$archivo_csv = fopen($ruta_salida . $nombre_csv, 'w');
+
+if ($archivo_nombre_paraderos === false) {
+  echo "No se pudo abrir archivo $archivo_nombre_paraderos".PHP_EOL;
+  exit(1);
+}
+
+if ($archivo_csv === false) {
+  echo "No se pudo crear archivo $ruta_salida/$nombre_csv.csv".PHP_EOL;
+  exit(1);
+}
+
+// se quita la primera linea y se agrega encabezado de csv
+$lineas = explode("\n", $contenido_csv);
+$lineas = array_slice($lineas, 1);
+$lineas = array_merge(array("Nombre latitud longitud".($hora==''?'':' hora')." centralidad"), $lineas);
+$contenido_csv = implode("\n", $lineas);
+
+// se lee linea por linea
+while (($linea = fgets($archivo_nombre_paraderos)) !== false) {
+  $elementos = array_map("trim", split(";", $linea));
+  
+  $patron = '/"'.$elementos[0].'"/i';
+  $nuevo_texto = '"'.$elementos[1].'",'.$elementos[2].','.$elementos[3].($hora==''?'':','.$hora);
+
+  $contenido_csv = preg_replace($patron, $nuevo_texto, $contenido_csv);
+}
+
+fclose($archivo_nombre_paraderos);
+
+fwrite($archivo_csv, $contenido_csv);
+fclose($archivo_csv);
+?>
