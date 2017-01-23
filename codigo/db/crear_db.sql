bdaf42eb9505209f2d9651f0c87b689241253dfa
root <root@ubuntu-2gb-sfo2-01-Tesis.localdomain>
2017-01-23 04:34:44 +0000

    nombre cambiado

--- codigo/db/crear_db.sql
+++ codigo/db/crear_db.sql
@@ -5,6 +5,6 @@ DROP DATABASE IF EXISTS memoria;
 DROP USER IF EXISTS felipe;
 
 -- Se crea usuario y base de datos
-CREATE USER felipe WITH PASSWORD 'felipe';
+CREATE USER felipe WITH PASSWORD 'tesis';
 CREATE DATABASE memoria OWNER felipe;
 
